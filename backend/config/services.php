d585166daa4005ee99487a1bf8c9c0f399750183
Your Name <you@example.com>
2026-01-05 10:12:40 +0100

    commit chaanges

--- backend/config/services.php
+++ backend/config/services.php
@@ -50,6 +50,9 @@ return [
     'onesignal' => [
         'app_id' => env('ONESIGNAL_APP_ID', '401c6128-33c4-47ba-af30-337413de42bb'),
         'rest_api_key' => env('ONESIGNAL_REST_API_KEY'),
+        'user_auth_key' => env('USER_AUTH_KEY', ''), // Optional - only needed for user management operations
+        'rest_api_url' => env('ONESIGNAL_REST_API_URL', 'https://api.onesignal.com'),
+        'guzzle_client_timeout' => env('ONESIGNAL_GUZZLE_CLIENT_TIMEOUT', 0),
     ],
 
 ];
